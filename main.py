4e65e3e6f3d6142057c4220081eba89d40a14c7c
MichaelKrechetov <krechetov_michael@i.ua>
2022-06-12 12:41:21 +0300

    first .kv

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 
 class MyGrid(Widget):
     pass
 
 
-class BalanceClockHandsApp(App):
+class BalanceClockHands(App):
     def build(self):
         return MyGrid()
 
 
 if __name__ == "__main__":
-    BalanceClockHandsApp().run()
+    BalanceClockHands().run()
 
