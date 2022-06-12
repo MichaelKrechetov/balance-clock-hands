b2dd955f8a586ccfee315e04715c62207d2b9a75
MichaelKrechetov <krechetov_michael@i.ua>
2022-06-12 11:28:58 +0300

    Beginning

---
+++ main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MyGrid(Widget):
+    pass
+
+
+class BalanceClockHandsApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    BalanceClockHandsApp().run()
+
